9b7c82e347a3bf696e142722935177ccaeceb3d1
Cameron Fabbri <cameronfabbri@gmail.com>
2018-01-20 16:23:41 -0600

    generated galaxies now saves out multiple for one galaxy

--- efigi/generate_test_galaxies.py
+++ efigi/generate_test_galaxies.py
@@ -1,6 +1,7 @@
 '''
 
-   This file generates celeba latent z vectors and the corresponding images such that the encoder can be trained
+   This file takes the test set attributes and generates 5 images from those with random z vectors
+   Each image saved out is the original test image on the left, then the next 5 columns are the generated
 
 '''
 import matplotlib.pyplot as plt
@@ -20,50 +21,53 @@ import cv2
 import os
 
 sys.path.insert(0, '../ops/')
+sys.path.insert(0, '../')
 
 from tf_ops import *
-import data_ops
 from nets import *
+import data_ops
 
 
 if __name__ == '__main__':
 
    parser = argparse.ArgumentParser()
    parser.add_argument('--CHECKPOINT_DIR', required=True,help='checkpoint directory',type=str)
-   parser.add_argument('--DATASET',        required=False,help='The DATASET to use',      type=str,default='galaxy')
    parser.add_argument('--OUTPUT_DIR',     required=False,help='Directory to save data', type=str,default='./')
-   parser.add_argument('--MAX_GEN',        required=False,help='Maximum images to generate',  type=int,default=5)
-   parser.add_argument('--SIZE',           required=False,help='Size of images', type=int,default=64)
+   parser.add_argument('--REDSHIFT',       required=True,help='Redshift or not', type=int,default=0)
    parser.add_argument('--DATA_DIR',       required=True,help='Data directory',type=str)
+   parser.add_argument('--MAX_GEN',        required=False,help='Maximum images to generate',  type=int,default=5)
    a = parser.parse_args()
 
    CHECKPOINT_DIR = a.CHECKPOINT_DIR
-   DATASET        = a.DATASET
    OUTPUT_DIR     = a.OUTPUT_DIR
-   MAX_GEN        = a.MAX_GEN
+   REDSHIFT       = bool(a.REDSHIFT)
    DATA_DIR       = a.DATA_DIR
-   SIZE           = a.SIZE
+   MAX_GEN        = a.MAX_GEN
 
    BATCH_SIZE = 64
+  
+   
+   print 'Loading data...'
+   train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_efigi(DATA_DIR, REDSHIFT, 64)
+   
+   y_dim = 4
+   if REDSHIFT: y_dim = 5
 
    try: os.makedirs(OUTPUT_DIR)
    except: pass
 
    # placeholders for data going into the network
    z           = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 100), name='z')
-   if DATASET == 'zoo': y_dim = 37
-   if DATASET == 'efigi': y_dim = 4
    y           = tf.placeholder(tf.float32, shape=(BATCH_SIZE, y_dim), name='y')
 
    # generated images
-   gen_images = netG(z, y, BATCH_SIZE, SIZE)
+   gen_images = netG(z, y, BATCH_SIZE, 64)
    
    saver = tf.train.Saver(max_to_keep=1)
    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
    sess  = tf.Session()
    sess.run(init)
    
-   # restore previous model if there is one
    ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
    if ckpt and ckpt.model_checkpoint_path:
       print "Restoring previous model..."
@@ -75,23 +79,23 @@ if __name__ == '__main__':
          raise
          exit()
 
-   print 'Loading data...'
-   if DATASET == 'zoo': train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_zoo(DATA_DIR, SIZE)
-   if DATASET == 'efigi': train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_efigi(DATA_DIR, SIZE)
    test_len = len(test_annots)
 
    print 'generating data...'
-   batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
+   batch_z = np.random.normal(0.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
    idx     = np.random.choice(np.arange(test_len), BATCH_SIZE, replace=False)
    batch_y = test_annots[idx]
 
-   gen_imgs = sess.run([gen_images], feed_dict={z:batch_z, y:batch_y})[0]
-
-   canvas = 255*np.ones((80, 80, 3), dtype=np.uint8)
+   gen_imgs = np.asarray(sess.run([gen_images], feed_dict={z:batch_z, y:batch_y})[0])
+   
+   # height, width, depth
+   canvas = 255*np.ones((84, MAX_GEN*74+10 , 3), dtype=np.uint8)
+   
    start_x = 10
    start_y = 10
    end_y = start_y+64
 
+   counter = 0
    for img in gen_imgs:
       img = (img+1.)
       img *= 127.5
@@ -99,10 +103,12 @@ if __name__ == '__main__':
       img = np.reshape(img, (64, 64, -1))
       end_x = start_x+64
       canvas[start_y:end_y, start_x:end_x, :] = img
-      break
-
-   print batch_y[0]
+      counter += 1
+      start_x = end_x + 10
+      if counter == MAX_GEN: break
    misc.imsave(OUTPUT_DIR+'generated.png', canvas)
+   exit()
+
    # batch y should be chosen from the test set
    exit()
    batch_y = np.random.choice([0, 1], size=(BATCH_SIZE,9))
