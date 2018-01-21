066fbd127f3616b1d21abdace2f8bb0c0dfbcfa7
Cameron Fabbri <cameronfabbri@gmail.com>
2018-01-20 23:38:45 -0600

    finished with generating galaxies

--- efigi/generate_test_galaxies.py
+++ efigi/generate_test_galaxies.py
@@ -44,12 +44,14 @@ if __name__ == '__main__':
    DATA_DIR       = a.DATA_DIR
    MAX_GEN        = a.MAX_GEN
 
-   BATCH_SIZE = 64
+   BATCH_SIZE = 1
   
    
    print 'Loading data...'
    train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_efigi(DATA_DIR, REDSHIFT, 64)
-   
+
+   print test_ids[0]
+
    y_dim = 4
    if REDSHIFT: y_dim = 5
 
@@ -80,62 +82,37 @@ if __name__ == '__main__':
          exit()
 
    test_len = len(test_annots)
+   print test_len,'testing images'
 
-   # TODO need to go through all test galaxies in batches
-   idx = 
-   for galaxy_id, annot in zip(test_ids, test_annots):
+   for t_img, t_annot, t_gid in zip(test_images, test_annots, test_ids):
+      #misc.imsave(OUTPUT_DIR+t_gid+'_.png',t_img)
+      canvas = 255*np.ones((84, (MAX_GEN+1)*74+10 , 3), dtype=np.uint8)
+      start_x = 10
+      start_y = 10
+      end_y = start_y+64
+      t_img = (t_img+1.)
+      t_img *= 127.5
+      t_img = np.clip(t_img, 0, 255).astype(np.uint8)
+      t_img = np.reshape(t_img, (64, 64, -1))
+      end_x = start_x+64
+      canvas[start_y:end_y, start_x:end_x, :] = t_img
+      start_x = end_x+10
+
+      # put a line of black pixels in between the real image and generated ones
+      canvas[:, end_x+5] = 0
+      
       for count in range(MAX_GEN):
          batch_z = np.random.normal(0.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-         batch_y = test_annots[idx]
-         idx     = idx + 1
-
-   #np.random.choice(np.arange(test_len), BATCH_SIZE, replace=False)
-
-   gen_imgs = np.asarray(sess.run([gen_images], feed_dict={z:batch_z, y:batch_y})[0])
-   
-   # height, width, depth
-   canvas = 255*np.ones((84, MAX_GEN*74+10 , 3), dtype=np.uint8)
-   
-   start_x = 10
-   start_y = 10
-   end_y = start_y+64
-
-   counter = 0
-   for img in gen_imgs:
-      img = (img+1.)
-      img *= 127.5
-      img = np.clip(img, 0, 255).astype(np.uint8)
-      img = np.reshape(img, (64, 64, -1))
-      end_x = start_x+64
-      canvas[start_y:end_y, start_x:end_x, :] = img
-      counter += 1
-      start_x = end_x + 10
-      if counter == MAX_GEN: break
-   misc.imsave(OUTPUT_DIR+'generated.png', canvas)
-   exit()
-
-   # batch y should be chosen from the test set
-   exit()
-   batch_y = np.random.choice([0, 1], size=(BATCH_SIZE,9))
-   batch_y[0][:] = 0
-   batch_y[0][-3] = male # make male or female
-   for i in range(14):
-      batch_y[0][i] = 1
-      print batch_y[0]
-      gen_imgs = sess.run([gen_images], feed_dict={z:batch_z, y:batch_y})[0]
-      for img in gen_imgs:
+         batch_y = np.expand_dims(t_annot, 0)
+         img = np.asarray(sess.run([gen_images], feed_dict={z:batch_z, y:batch_y})[0])[0]
          img = (img+1.)
          img *= 127.5
          img = np.clip(img, 0, 255).astype(np.uint8)
          img = np.reshape(img, (64, 64, -1))
-         break
-      end_x = start_x+64
-      canvas[start_y:end_y, start_x:end_x, :] = img
-      start_x += 64+10
-   
-      batch_y = np.random.choice([0, 1], size=(BATCH_SIZE,9))
-      batch_y[0][:] = 0
-      batch_y[0][-3] = male # make male or female
-
-   misc.imsave(OUTPUT_DIR+'attributes.png', canvas)
+         
+         end_x = start_x+64
+         canvas[start_y:end_y, start_x:end_x, :] = img
+         start_x = end_x + 10
+         
+      misc.imsave(OUTPUT_DIR+t_gid+'.png', canvas)
    exit()
