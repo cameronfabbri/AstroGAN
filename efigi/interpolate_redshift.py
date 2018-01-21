1ee94b20beba14a51e6f60f3ea8233150d8c95ae
Cameron Fabbri <cameronfabbri@gmail.com>
2018-01-21 11:52:27 -0600

    updates

--- efigi/interpolate_redshift.py
+++ efigi/interpolate_redshift.py
@@ -82,8 +82,8 @@ if __name__ == '__main__':
    batch_y = test_annots[idx]
    batch_y[:NUM+1] = batch_y[1] # gotta make sure they have the same attributes
 
-   batch_y[0][-1] = 0.001
-   batch_y[1][-1] = 0.1
+   batch_y[0][-1] = 2.635e-05
+   batch_y[1][-1] = 0.08245
    print batch_y[0]
    print batch_y[1]
 
