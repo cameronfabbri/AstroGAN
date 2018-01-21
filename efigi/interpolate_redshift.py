23c296a26a5472f75de9c5b5d7c9691172c4fa0f
Cameron Fabbri <cameronfabbri@gmail.com>
2018-01-21 12:00:19 -0600

    updates

--- efigi/interpolate_redshift.py
+++ efigi/interpolate_redshift.py
@@ -90,8 +90,11 @@ if __name__ == '__main__':
 
    batch_y[1] = batch_y[0]
 
-   batch_y[0][-1] = 2.635e-05
-   batch_y[1][-1] = 0.08245
+   # min and max from dataset
+   #batch_y[0][-1] = 2.635e-05
+   #batch_y[1][-1] = 0.08245
+   batch_y[0][-1] = 0.1
+   batch_y[1][-1] = 0.5
    print batch_y[0]
    print batch_y[1]
 
