1fca341d81c539615595d278d00b3bbca7e882c6
Cameron Fabbri <cameronfabbri@gmail.com>
2018-01-22 15:06:46 -0600

    changed around to work with many attributes now

--- efigi/generate_specific.py
+++ efigi/generate_specific.py
@@ -26,33 +26,28 @@ from tf_ops import *
 from nets import *
 import data_ops
 
+from config import classes
 
 if __name__ == '__main__':
+   
+   pkl_file = open(sys.argv[1], 'rb')
+   a = pickle.load(pkl_file)
+   print a
+  
+   OUTPUT_DIR = sys.argv[2]
+   MAX_GEN    = int(sys.argv[3])
 
-   parser = argparse.ArgumentParser()
-   parser.add_argument('--CHECKPOINT_DIR', required=True,help='checkpoint directory',type=str)
-   parser.add_argument('--OUTPUT_DIR',     required=False,help='Directory to save data', type=str,default='./')
-   parser.add_argument('--REDSHIFT',       required=True,help='Redshift or not', type=int,default=0)
-   parser.add_argument('--DATA_DIR',       required=True,help='Data directory',type=str)
-   parser.add_argument('--MAX_GEN',        required=False,help='Maximum images to generate',  type=int,default=5)
-   a = parser.parse_args()
-
-   CHECKPOINT_DIR = a.CHECKPOINT_DIR
-   OUTPUT_DIR     = a.OUTPUT_DIR
-   REDSHIFT       = bool(a.REDSHIFT)
-   DATA_DIR       = a.DATA_DIR
-   MAX_GEN        = a.MAX_GEN
+   CHECKPOINT_DIR = a['CHECKPOINT_DIR']
+   DATA_DIR       = a['DATA_DIR']
+   CLASSES        = a['CLASSES']
+   LOSS           = a['LOSS']
 
    BATCH_SIZE = 1
-  
    
    print 'Loading data...'
-   train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_efigi(DATA_DIR, REDSHIFT, 64)
+   train_images, train_annots, train_ids, test_images, test_annots, test_ids = data_ops.load_efigi(DATA_DIR, CLASSES, 64)
 
-   print test_ids[0]
-
-   y_dim = 4
-   if REDSHIFT: y_dim = 5
+   y_dim = 18
 
    try: os.makedirs(OUTPUT_DIR)
    except: pass
@@ -84,14 +79,45 @@ if __name__ == '__main__':
    print test_len,'testing images'
 
    # create attributes here
-   arm_strength = 1.0
-   arm_curve    = 0.25
-   dust         = 0.0
-   mult         = 0.0
-   redshift     = 2.18
-   batch_y = np.expand_dims(np.asarray([arm_strength, arm_curve, dust, mult, redshift]), 0)
+   T               = 0.
+   bulge_to_total  = 0.
+   arm_strength    = 1.0
+   arm_curve       = 0.25
+   arm_rotation    = 0.
+   bar_length      = 0.
+   inner_ring      = 0.
+   outer_ring      = 0.
+   pseudo_ring     = 0.
+   perturbation    = 0.
+   visible_dust    = 0.
+   dust_dispertion = 0.
+   flocculence     = 0.
+   hot_spots       = 0.
+   inclination     = 0.
+   contamination   = 0.
+   mult            = 0.
+   redshift        = 2.18
 
-   for n in range(MAX_GEN):
+   batch_y = np.expand_dims(np.asarray([T,
+                                        bulge_to_total,
+                                        arm_strength,
+                                        arm_curve,
+                                        arm_rotation,
+                                        bar_length,
+                                        inner_ring,
+                                        outer_ring,
+                                        pseudo_ring,
+                                        perturbation,
+                                        visible_dust,
+                                        dust_dispertion,
+                                        flocculence,
+                                        hot_spots,
+                                        inclination,
+                                        contamination,
+                                        mult,
+                                        redshift]), 0)
+
+   for n in tqdm(range(MAX_GEN)):
       batch_z = np.random.normal(0.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
       img = np.asarray(sess.run([gen_images], feed_dict={z:batch_z, y:batch_y})[0])[0]
       misc.imsave(OUTPUT_DIR+str(n)+'.png',img)
