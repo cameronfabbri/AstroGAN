61d4b403c4c222f8b537f22f9e2e39f0e84b729d
Cameron Fabbri <cameronfabbri@gmail.com>
2018-01-26 11:42:49 -0600

    added skip connections

--- galaxyzoo/superres/nets.py
+++ galaxyzoo/superres/nets.py
@@ -44,32 +44,32 @@ def netG(x):
    # decoder, no batch norm
    dec_conv1 = tcl.convolution2d_transpose(enc_conv6, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv1')
    dec_conv1 = relu(dec_conv1)
-   #dec_conv1 = tf.concat([dec_conv1, enc_conv3], axis=3)
+   dec_conv1 = tf.concat([dec_conv1, enc_conv5], axis=3)
    print 'dec_conv1:',dec_conv1
 
    dec_conv2 = tcl.convolution2d_transpose(dec_conv1, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv2')
    dec_conv2 = relu(dec_conv2)
-   #dec_conv2 = tf.concat([dec_conv2, enc_conv2], axis=3)
+   dec_conv2 = tf.concat([dec_conv2, enc_conv4], axis=3)
    print 'dec_conv2:',dec_conv2
    
    dec_conv3 = tcl.convolution2d_transpose(dec_conv2, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv3')
    dec_conv3 = relu(dec_conv3)
-   #dec_conv3 = tf.concat([dec_conv3, enc_conv1], axis=3)
+   dec_conv3 = tf.concat([dec_conv3, enc_conv3], axis=3)
    print 'dec_conv3:',dec_conv3
 
    dec_conv4 = tcl.convolution2d_transpose(dec_conv3, 512, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv4')
    dec_conv4 = relu(dec_conv4)
-   #dec_conv4 = tf.concat([dec_conv4, x], axis=3)
+   dec_conv4 = tf.concat([dec_conv4, enc_conv2], axis=3)
    print 'dec_conv4:',dec_conv4
    
    dec_conv5 = tcl.convolution2d_transpose(dec_conv4, 256, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv5')
    dec_conv5 = relu(dec_conv5)
-   #dec_conv5 = tf.concat([dec_conv5, enc_conv3], axis=3)
+   dec_conv5 = tf.concat([dec_conv5, enc_conv1], axis=3)
    print 'dec_conv5:',dec_conv5
 
    dec_conv6 = tcl.convolution2d_transpose(dec_conv5, 128, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv6')
    dec_conv6 = relu(dec_conv6)
-   #dec_conv6 = tf.concat([dec_conv6, enc_conv2], axis=3)
+   dec_conv6 = tf.concat([dec_conv6, x], axis=3)
    print 'dec_conv6:',dec_conv6
    
    dec_conv7 = tcl.convolution2d_transpose(dec_conv6, 64, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv7')
@@ -80,7 +80,6 @@ def netG(x):
    dec_conv8 = tcl.convolution2d_transpose(dec_conv7, 3, 4, 2, activation_fn=tf.identity, weights_initializer=tf.random_normal_initializer(stddev=0.02), scope='g_dec_conv8')
    dec_conv8 = tanh(dec_conv8)
    print 'dec_conv8', dec_conv8
-
    return dec_conv8
 
 
